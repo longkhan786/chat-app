8fcb1c9f4f4cf90bb1e22793858372744ed00e38
longkhan786 <longkhandhareja78@gmail.com>
2025-05-28 21:20:15 +0530

    Initial commit

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/longkhan786/chat-app/bootstrap"
+)
+
+func main()  {
+	app := bootstrap.App()
+ 	env := app.Env
+	fmt.Println("App Environment:", env.AppEnv)
+}
