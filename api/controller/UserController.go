321c5ea74a84a8d0e3627ac35a8a99def7402daa
longkhan786 <longkhandhareja78@gmail.com>
2025-06-04 12:10:07 +0530

    message content save

--- api/controller/UserController.go
+++ api/controller/UserController.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/longkhan786/chat-app/bootstrap"
+	"github.com/longkhan786/chat-app/model"
 	"gorm.io/gorm"
 )
 
@@ -21,7 +22,20 @@ func (uc *UserController) Signup(c *gin.Context) {
 func (uc *UserController) SendMessage(c *gin.Context) {
 
 	message := c.PostForm("message")
+
+	userMessage := model.UserMessage{
+		SenderID:  	1,
+		ReceiverID: 2,
+		Content:   	message,
+	}
+
+	db := uc.Db
+
+	db.Create(&userMessage)
+	db.Preload("Sender").Preload("Receiver").First(&userMessage, userMessage.ID)
+	
 	c.JSON(200, gin.H{
-		"message": "Message sent: " + message,
+		"message": "Message sent successfully",
+		"data":    userMessage,
 	})
 }
