4fffa0822890dded137eae05e649f5aa18580275
longkhan786 <longkhandhareja78@gmail.com>
2025-06-04 14:16:14 +0530

    feat: restructure Tailwind CSS integration and update dependencies

--- template/app/root.tsx
+++ template/app/root.tsx
@@ -8,6 +8,7 @@ import {
 } from "react-router";
 
 import type { Route } from "./+types/root";
+import './tailwind.css';
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
--- template/app/welcome/welcome.tsx
+++ template/app/welcome/welcome.tsx
@@ -1,7 +1,9 @@
 export function Welcome() {
   return (
     <main>
-      <h1>Chat app frontend</h1>
+      <h1 className="tw-text-3xl tw-font-bold tw-underline">
+        Hello world!
+      </h1>
     </main>
   );
 }
